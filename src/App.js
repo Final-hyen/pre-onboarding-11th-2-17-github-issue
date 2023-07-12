d9a74acb069a0250736045cde8b22cb3c24a6635
Final Hyen <jwx9797@gmail.com>
2023-07-12 17:50:28 +0900

    Test : var test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const coffee = 3;
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React `${coffee}`
         </a>
       </header>
     </div>
