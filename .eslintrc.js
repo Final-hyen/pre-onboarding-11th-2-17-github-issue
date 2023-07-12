321a9b47a8682d325b05412a3ebb64abf5f8c4ef
Final Hyen <jwx9797@gmail.com>
2023-07-12 17:25:36 +0900

    Test

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'no-console': 'off',
+    'no-alert': 'off',
+  },
+};
